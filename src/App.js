c9f9639a22bbb4ca6d10e1c4a165121e5365f2ef
mustafa-tola <46423331+mustafa-tola@users.noreply.github.com>
2020-06-09 13:31:26 +0500

    Adding css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Hello from "./Hello";
 
 function App({ name, age }) {
   return (
     <div>
       Hello From App.js updated {name} Age = {
       age - 5}
+      <br />
+      <Hello firstName={name}/>
     </div>
   )
 };
